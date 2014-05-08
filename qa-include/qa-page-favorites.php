ba126ab7257fad3dd49a3c5d2cc102a034cf2d03
Scott <scott.vivian@gmail.com>
2014-05-08 16:42:17 +0100

    Refactor qa-page-favorites

--- qa-include/qa-page-favorites.php
+++ qa-include/qa-page-favorites.php
@@ -35,7 +35,7 @@
 
 //	Check that we're logged in
 
-	$userid=qa_get_logged_in_userid();
+	$userid = qa_get_logged_in_userid();
 
 	if (!isset($userid))
 		qa_redirect('login');
@@ -43,52 +43,51 @@
 
 //	Get lists of favorites for this user
 
-	list($questions, $users, $tags, $categories)=qa_db_select_with_pending(
+	list($questions, $users, $tags, $categories) = qa_db_select_with_pending(
 		qa_db_user_favorite_qs_selectspec($userid),
 		QA_FINAL_EXTERNAL_USERS ? null : qa_db_user_favorite_users_selectspec($userid),
 		qa_db_user_favorite_tags_selectspec($userid),
 		qa_db_user_favorite_categories_selectspec($userid)
 	);
 
-	$usershtml=qa_userids_handles_html(QA_FINAL_EXTERNAL_USERS ? $questions : array_merge($questions, $users));
+	$usershtml = qa_userids_handles_html(QA_FINAL_EXTERNAL_USERS ? $questions : array_merge($questions, $users));
 
 
 //	Prepare and return content for theme
 
-	$qa_content=qa_content_prepare(true);
+	$qa_content = qa_content_prepare(true);
 
-	$qa_content['title']=qa_lang_html('misc/my_favorites_title');
+	$qa_content['title'] = qa_lang_html('misc/my_favorites_title');
 
 
 //	Favorite questions
 
-	$qa_content['q_list']=array(
+	$qa_content['q_list'] = array(
 		'title' => count($questions) ? qa_lang_html('main/nav_qs') : qa_lang_html('misc/no_favorite_qs'),
-
 		'qs' => array(),
 	);
 
 	if (count($questions)) {
-		$qa_content['q_list']['form']=array(
+		$qa_content['q_list']['form'] = array(
 			'tags' => 'method="post" action="'.qa_self_html().'"',
-
 			'hidden' => array(
 				'code' => qa_get_form_security_code('vote'),
 			),
 		);
 
-		$defaults=qa_post_html_defaults('Q');
+		$defaults = qa_post_html_defaults('Q');
 
-		foreach ($questions as $question)
-			$qa_content['q_list']['qs'][]=qa_post_html_fields($question, $userid, qa_cookie_get(),
+		foreach ($questions as $question) {
+			$qa_content['q_list']['qs'][] = qa_post_html_fields($question, $userid, qa_cookie_get(),
 				$usershtml, null, qa_post_html_options($question, $defaults));
+		}
 	}
 
 
 //	Favorite users
 
 	if (!QA_FINAL_EXTERNAL_USERS) {
-		$qa_content['ranking_users']=array(
+		$qa_content['ranking_users'] = array(
 			'title' => count($users) ? qa_lang_html('main/nav_users') : qa_lang_html('misc/no_favorite_users'),
 			'items' => array(),
 			'rows' => ceil(count($users)/qa_opt('columns_users')),
@@ -99,7 +98,7 @@
 			$avatarhtml = qa_get_user_avatar_html($user['flags'], $user['email'], $user['handle'],
 					$user['avatarblobid'], $user['avatarwidth'], $user['avatarheight'], qa_opt('avatar_users_size'), true);
 
-			$qa_content['ranking_users']['items'][]=array(
+			$qa_content['ranking_users']['items'][] = array(
 				'avatar' => $avatarhtml,
 				'label' => $usershtml[$user['userid']],
 				'score' => qa_html(number_format($user['points'])),
@@ -112,50 +111,53 @@
 //	Favorite tags
 
 	if (qa_using_tags()) {
-		$qa_content['ranking_tags']=array(
+		$qa_content['ranking_tags'] = array(
 			'title' => count($tags) ? qa_lang_html('main/nav_tags') : qa_lang_html('misc/no_favorite_tags'),
 			'items' => array(),
 			'rows' => ceil(count($tags)/qa_opt('columns_tags')),
 			'type' => 'tags'
 		);
 
-		foreach ($tags as $tag)
-			$qa_content['ranking_tags']['items'][]=array(
+		foreach ($tags as $tag) {
+			$qa_content['ranking_tags']['items'][] = array(
 				'label' => qa_tag_html($tag['word'], false, true),
 				'count' => number_format($tag['tagcount']),
 			);
+		}
 	}
 
 
 //	Favorite categories
 
 	if (qa_using_categories()) {
-		$qa_content['nav_list_categories']=array(
+		$qa_content['nav_list_categories'] = array(
 			'title' => count($categories) ? qa_lang_html('main/nav_categories') : qa_lang_html('misc/no_favorite_categories'),
 			'nav' => array(),
 			'type' => 'browse-cat',
 		);
 
-		foreach ($categories as $category)
-			$qa_content['nav_list_categories']['nav'][$category['categoryid']]=array(
+		foreach ($categories as $category) {
+			$cat_url = qa_path_html( 'questions/' . implode( '/', array_reverse(explode('/', $category['backpath'])) ) );
+			$cat_anchor = $category['qcount'] == 1
+				? qa_lang_html_sub('main/1_question', '1', '1')
+				: qa_lang_html_sub('main/x_questions', number_format($category['qcount']));
+			$cat_descr = strlen($category['content']) ? qa_html(' - '.$category['content']) : '';
+
+			$qa_content['nav_list_categories']['nav'][$category['categoryid']] = array(
 				'label' => qa_html($category['title']),
 				'state' => 'open',
 				'favorited' => true,
-				'note' => ' - <a href="'.qa_path_html('questions/'.implode('/', array_reverse(explode('/', $category['backpath'])))).'">'.
-					( ($category['qcount']==1)
-						? qa_lang_html_sub('main/1_question', '1', '1')
-						: qa_lang_html_sub('main/x_questions', number_format($category['qcount']))
-					).'</a>'.
-					(strlen($category['content']) ? qa_html(' - '.$category['content']) : ''),
+				'note' => ' - <a href="'.$cat_url.'">'.$cat_anchor.'</a>'.$cat_descr,
 			);
+		}
 	}
 
 
 //	Sub navigation for account pages and suggestion
 
-	$qa_content['suggest_next']=qa_lang_html_sub('misc/suggest_favorites_add', '<span class="qa-favorite-image">&nbsp;</span>');
+	$qa_content['suggest_next'] = qa_lang_html_sub('misc/suggest_favorites_add', '<span class="qa-favorite-image">&nbsp;</span>');
 
-	$qa_content['navigation']['sub']=qa_user_sub_navigation(qa_get_logged_in_handle(), 'favorites', true);
+	$qa_content['navigation']['sub'] = qa_user_sub_navigation(qa_get_logged_in_handle(), 'favorites', true);
 
 
 	return $qa_content;
