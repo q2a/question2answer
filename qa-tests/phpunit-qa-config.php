72e096917f44e12de18e7c381ef056f3f7d16ba3
Scott <scott.vivian@gmail.com>
2014-12-01 13:53:09 +0000

    Travis CI: turn off Code Sniffer/Mess Detector
    They are creating way too many errors that are not going to be fixed
    right now.
    Also added basic qa-config.php so that PHPUnit runs without error.

---
+++ qa-tests/phpunit-qa-config.php
@@ -0,0 +1,10 @@
+<?php
+// Stand-in config file for PHPUnit
+
+define('QA_MYSQL_HOSTNAME', '');
+define('QA_MYSQL_USERNAME', '');
+define('QA_MYSQL_PASSWORD', '');
+define('QA_MYSQL_DATABASE', '');
+
+define('QA_MYSQL_TABLE_PREFIX', 'qa_');
+define('QA_EXTERNAL_USERS', false);
