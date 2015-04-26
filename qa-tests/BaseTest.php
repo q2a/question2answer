e082644558530070e960508d4491e8529355b911
pupi1985 <pupi1985@bitbucket.org>
2015-04-25 21:26:04 -0300

    Refactored some byte handling operations

--- qa-tests/BaseTest.php
+++ qa-tests/BaseTest.php
@@ -22,4 +22,32 @@ class BaseTest extends PHPUnit_Framework_TestCase
 		$test = qa_js(true, true);
 		$this->assertSame("'true'", $test);
 	}
+
+	public function test__convert_to_bytes()
+	{
+		$test = convert_to_bytes('k', 100);
+		$this->assertSame(102400, $test);
+
+		$test = convert_to_bytes('m', 100);
+		$this->assertSame(104857600, $test);
+
+		$test = convert_to_bytes('g', 100);
+		$this->assertSame(107374182400, $test);
+
+		$test = convert_to_bytes('K', 100);
+		$this->assertSame(102400, $test);
+
+		$test = convert_to_bytes('M', 100);
+		$this->assertSame(104857600, $test);
+
+		$test = convert_to_bytes('G', 100);
+		$this->assertSame(107374182400, $test);
+
+		$test = convert_to_bytes('', 100);
+		$this->assertSame(100, $test);
+
+		$test = convert_to_bytes('k', 1024);
+		$this->assertSame(1048576, $test);
+	}
+
 }
