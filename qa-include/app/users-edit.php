c77efc5762e61327c333a7a74ee680ac7da5839d
tobias <tobias.florian.marx@gmail.com>
2015-05-25 11:08:45 +0200

    - additional method to create a user with specify primary key

--- qa-include/app/users-edit.php
+++ qa-include/app/users-edit.php
@@ -144,14 +144,21 @@
 		return array();
 	}
 
+/*
+ * TODO: // add qa_create_new_user($uid, $email, $password, $handle, $level=QA_USER_LEVEL_BASIC, $confirmed=false)
+ */
 
-	function qa_create_new_user($email, $password, $handle, $level=QA_USER_LEVEL_BASIC, $confirmed=false)
+	function qa_create_new_user_with_id($uid, $email, $password, $handle)
 /*
 	Create a new user (application level) with $email, $password, $handle and $level.
 	Set $confirmed to true if the email address has been confirmed elsewhere.
 	Handles user points, notification and optional email confirmation.
 */
 	{
+
+        $level=QA_USER_LEVEL_BASIC;
+        $confirmed=false;
+
 		if (qa_to_override(__FUNCTION__)) { $args=func_get_args(); return qa_call_override(__FUNCTION__, $args); }
 
 		require_once QA_INCLUDE_DIR.'db/users.php';
@@ -160,46 +167,101 @@
 		require_once QA_INCLUDE_DIR.'app/emails.php';
 		require_once QA_INCLUDE_DIR.'app/cookies.php';
 
-		$userid=qa_db_user_create($email, $password, $handle, $level, qa_remote_ip_address());
-		qa_db_points_update_ifuser($userid, null);
+		qa_db_user_create_with_id($uid, $email, $password, $handle, $level, qa_remote_ip_address());
+		qa_db_points_update_ifuser($uid, null);
 		qa_db_uapprovecount_update();
 
 		if ($confirmed)
-			qa_db_user_set_flag($userid, QA_USER_FLAGS_EMAIL_CONFIRMED, true);
+			qa_db_user_set_flag($uid, QA_USER_FLAGS_EMAIL_CONFIRMED, true);
 
 		if (qa_opt('show_notice_welcome'))
-			qa_db_user_set_flag($userid, QA_USER_FLAGS_WELCOME_NOTICE, true);
+			qa_db_user_set_flag($uid, QA_USER_FLAGS_WELCOME_NOTICE, true);
 
 		$custom=qa_opt('show_custom_welcome') ? trim(qa_opt('custom_welcome')) : '';
 
 		if (qa_opt('confirm_user_emails') && ($level<QA_USER_LEVEL_EXPERT) && !$confirmed) {
 			$confirm=strtr(qa_lang('emails/welcome_confirm'), array(
-				'^url' => qa_get_new_confirm_url($userid, $handle)
+				'^url' => qa_get_new_confirm_url($uid, $handle)
 			));
 
 			if (qa_opt('confirm_user_required'))
-				qa_db_user_set_flag($userid, QA_USER_FLAGS_MUST_CONFIRM, true);
+				qa_db_user_set_flag($uid, QA_USER_FLAGS_MUST_CONFIRM, true);
 
 		} else
 			$confirm='';
 
 		if (qa_opt('moderate_users') && qa_opt('approve_user_required') && ($level<QA_USER_LEVEL_EXPERT))
-			qa_db_user_set_flag($userid, QA_USER_FLAGS_MUST_APPROVE, true);
+			qa_db_user_set_flag($uid, QA_USER_FLAGS_MUST_APPROVE, true);
 
-		qa_send_notification($userid, $email, $handle, qa_lang('emails/welcome_subject'), qa_lang('emails/welcome_body'), array(
+		qa_send_notification($uid, $email, $handle, qa_lang('emails/welcome_subject'), qa_lang('emails/welcome_body'), array(
 			'^password' => isset($password) ? qa_lang('main/hidden') : qa_lang('users/password_to_set'), // v 1.6.3: no longer email out passwords
 			'^url' => qa_opt('site_url'),
 			'^custom' => strlen($custom) ? ($custom."\n\n") : '',
 			'^confirm' => $confirm,
 		));
 
-		qa_report_event('u_register', $userid, $handle, qa_cookie_get(), array(
+		qa_report_event('u_register', $uid, $handle, qa_cookie_get(), array(
 			'email' => $email,
 			'level' => $level,
 		));
 
-		return $userid;
 	}
+
+function qa_create_new_user($email, $password, $handle, $level=QA_USER_LEVEL_BASIC, $confirmed=false)
+    /*
+        Create a new user (application level) with $email, $password, $handle and $level.
+        Set $confirmed to true if the email address has been confirmed elsewhere.
+        Handles user points, notification and optional email confirmation.
+    */
+{
+    if (qa_to_override(__FUNCTION__)) { $args=func_get_args(); return qa_call_override(__FUNCTION__, $args); }
+
+    require_once QA_INCLUDE_DIR.'db/users.php';
+    require_once QA_INCLUDE_DIR.'db/points.php';
+    require_once QA_INCLUDE_DIR.'app/options.php';
+    require_once QA_INCLUDE_DIR.'app/emails.php';
+    require_once QA_INCLUDE_DIR.'app/cookies.php';
+
+    $userid=qa_db_user_create($email, $password, $handle, $level, qa_remote_ip_address());
+    qa_db_points_update_ifuser($userid, null);
+    qa_db_uapprovecount_update();
+
+    if ($confirmed)
+        qa_db_user_set_flag($userid, QA_USER_FLAGS_EMAIL_CONFIRMED, true);
+
+    if (qa_opt('show_notice_welcome'))
+        qa_db_user_set_flag($userid, QA_USER_FLAGS_WELCOME_NOTICE, true);
+
+    $custom=qa_opt('show_custom_welcome') ? trim(qa_opt('custom_welcome')) : '';
+
+    if (qa_opt('confirm_user_emails') && ($level<QA_USER_LEVEL_EXPERT) && !$confirmed) {
+        $confirm=strtr(qa_lang('emails/welcome_confirm'), array(
+            '^url' => qa_get_new_confirm_url($userid, $handle)
+        ));
+
+        if (qa_opt('confirm_user_required'))
+            qa_db_user_set_flag($userid, QA_USER_FLAGS_MUST_CONFIRM, true);
+
+    } else
+        $confirm='';
+
+    if (qa_opt('moderate_users') && qa_opt('approve_user_required') && ($level<QA_USER_LEVEL_EXPERT))
+        qa_db_user_set_flag($userid, QA_USER_FLAGS_MUST_APPROVE, true);
+
+    qa_send_notification($userid, $email, $handle, qa_lang('emails/welcome_subject'), qa_lang('emails/welcome_body'), array(
+        '^password' => isset($password) ? qa_lang('main/hidden') : qa_lang('users/password_to_set'), // v 1.6.3: no longer email out passwords
+        '^url' => qa_opt('site_url'),
+        '^custom' => strlen($custom) ? ($custom."\n\n") : '',
+        '^confirm' => $confirm,
+    ));
+
+    qa_report_event('u_register', $userid, $handle, qa_cookie_get(), array(
+        'email' => $email,
+        'level' => $level,
+    ));
+
+    return $userid;
+}
 
 
 	function qa_delete_user($userid)
