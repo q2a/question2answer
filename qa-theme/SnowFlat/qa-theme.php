f10736ae1ab9817c582e669bfca3bcc0a4e8ba96
Christian Pietsch <chr.pietsch+github@googlemail.com>
2015-10-03 13:43:34 +0200

    use absolute URL, or it won't work

--- qa-theme/SnowFlat/qa-theme.php
+++ qa-theme/SnowFlat/qa-theme.php
@@ -67,7 +67,7 @@ class qa_html_theme extends qa_html_theme_base
 			$this->content['css_src'][] = $this->rooturl . 'qa-styles-rtl.css?' . QA_VERSION;
 
 		// add Ubuntu font CSS file
-		$this->content['css_src'][] = 'font/ubuntu.css';
+		$this->content['css_src'][] = $this->rooturl . 'font/ubuntu.css';
 
 		parent::head_css();
 
