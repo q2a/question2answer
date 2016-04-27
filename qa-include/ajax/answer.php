6c99c7de4b661796dd0b0197fddb6b283a211b58
q2apro.com <info@q2apro.com>
2016-04-27 14:50:28 +0300

    Fix to prevent losing of content
    Read problematic scenario at http://www.question2answer.org/qa/26327/ - confirmed by many of my angry users...

--- qa-include/ajax/answer.php
+++ qa-include/ajax/answer.php
@@ -38,7 +38,7 @@
 
 //	Check if the question exists, is not closed, and whether the user has permission to do this
 
-	if ((@$question['basetype']=='Q') && (!isset($question['closedbyid'])) && !qa_user_post_permit_error('permit_post_a', $question, QA_LIMIT_ANSWERS)) {
+	if ((@$question['basetype']=='Q') && !qa_user_post_permit_error('permit_post_a', $question, QA_LIMIT_ANSWERS)) {
 		require_once QA_INCLUDE_DIR.'app/captcha.php';
 		require_once QA_INCLUDE_DIR.'app/format.php';
 		require_once QA_INCLUDE_DIR.'app/post-create.php';
