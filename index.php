1de770c54afc396888559c7ff9dc696f514d3933
freeman-c <freeman.csc@gmail.com>
2016-07-22 10:23:49 +0300

    index changed - duplicates
    stupid things performed

--- index.php
+++ index.php
@@ -25,7 +25,27 @@
 	define('QA_BASE_DIR', dirname(empty($_SERVER['SCRIPT_FILENAME']) ? __FILE__ : $_SERVER['SCRIPT_FILENAME']).'/');
 
 	require 'qa-include/qa-index.php';
-
+	require 'qa-include/qa-index.php';
+	require 'qa-include/qa-index.php';
+	require 'qa-include/qa-index.php';
+	require 'qa-include/qa-index.php';
+	require 'qa-include/qa-index.php';
+	require 'qa-include/qa-index.php';
+	require 'qa-include/qa-index.php';
+	require 'qa-include/qa-index.php';
+	require 'qa-include/qa-index.php';
+	require 'qa-include/qa-index.php';
+	require 'qa-include/qa-index.php';
+	require 'qa-include/qa-index.php';
+	require 'qa-include/qa-index.php';
+	require 'qa-include/qa-index.php';
+	require 'qa-include/qa-index.php';
+	require 'qa-include/qa-index.php';
+	require 'qa-include/qa-index.php';
+	require 'qa-include/qa-index.php';
+	require 'qa-include/qa-index.php';
+	require 'qa-include/qa-index.php';
+    
 
 /*
 	Omit PHP closing tag to help avoid accidental output
