268cd3fb21109bd32577f075dd0b75dd768b44c4
Larry Lewis <larry@jenolan.org>
2016-09-09 12:55:13 +1000

    Issue #447 handle login, logout & register page calls when external auth is enabled

--- qa-include/pages/logout.php
+++ qa-include/pages/logout.php
@@ -26,8 +26,14 @@ if (!defined('QA_VERSION')) { // don't allow this page to be requested directly 
 }
 
 
-if (QA_FINAL_EXTERNAL_USERS)
-	qa_fatal_error('User logout is handled by external code');
+if (QA_FINAL_EXTERNAL_USERS) {
+	$request=qa_request();
+	$topath=qa_get('to'); // lets user switch between login and register without losing destination page
+	$userlinks=qa_get_login_links(qa_path_to_root(), isset($topath) ? $topath : qa_path($request, $_GET, ''));
+	if (!empty($userlinks['logout']))
+		qa_redirect_raw($userlinks['logout']);
+	qa_fatal_error('User logout should be handled by external code');
+}
 
 if (qa_is_logged_in())
 	qa_set_logged_in_user(null);
