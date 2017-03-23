b4be25ebd0dcd64c68ca97a9183ee58b665bd51b
Giorgi Tsomaia <comaiagiorgi@gmail.com>
2017-03-23 16:39:57 +0400

    Full translation into Georgian (ka)
    These are all the lang files translated into Georgian by me, Giorgi Tsomaia.

---
+++ qa-lang/ka-GE/qa-lang-question.php
@@ -0,0 +1,183 @@
+<?php
+/*
+	Question2Answer by Gideon Greenspan and contributors
+	http://www.question2answer.org/
+
+	File: qa-include/qa-lang-question.php
+	Description: Language phrases for question page
+    TRANSLATED INTO GEORGIAN BY GIORGI TSOMAIA
+
+Done:95%
+Checked:0%
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+	return array(
+		'1_answer_title' => '1 პასუხი',
+		'a_convert_to_c' => 'გადააქციე ეს პასუხი კომენტარად',
+		'a_convert_to_c_on' => 'გადააქციე ეს პასუხი კომენტარად აქ:',
+		'a_convert_warn' => 'გაფრთხილება: ამ დიალოგის უკან დაბრუნება შეუძლებელია.',
+		'a_convert_warn_cs' => 'Warning: This conversion cannot be reversed and will also move this answer\'s comments.', // სათარგმნი
+		'a_notify_email' => 'მომწერე ამ მისამართზე თუკი ჩემს პასუხს შეარჩევენ ან მასზე დააკომენტარებენ:',
+		'a_notify_label' => 'მომწერე თუკი ჩემს პასუხს შეარჩევენ ან მასზე დააკომენტარებენ',
+		'a_notify_x_label' => 'მომწერე (^) თუკი ჩემს პასუხს შეარჩევენ ან მასზე დააკომენტარებენ',
+		'a_waiting_your_approval' => 'ეს პასუხი ელოდება თქვენს დასტურს',
+		'a_your_waiting_approval' => 'თქვენი პასუხი შემოწმდება და დადასტურდება ძალიან მალე.',
+		'add_answer_button' => 'დაწერე პასუხი',
+		'add_comment_button' => 'დაწერე კომენტარი',
+		'add_q_favorites' => 'დაამატე ეს შეკითხვა ფავორიტებში',
+		'anon_name_label' => 'თქვენი სახელი, რომელიც გამოჩნდება (არასავალდებულო):',
+		'answer_button' => 'პასუხი',
+		'answer_limit' => 'მიღებულია ძალიან ბევრი პასუხი - გთხოვთ სცადოთ ხელახლა ერთი საათის შემდეგ',
+		'answer_must_be_approved' => 'შეკითხვაზე პასუხის გაცემისთვის აუცილებელია თქვენი ანგარიში იყოს გააქტიურებული.',
+		'answer_must_confirm' => 'შეკითხვაზე პასუხის გაცემისთვის გთხოვთ, ^5დაადასტურეთ თქვენი ელ-ფოსტის მისამართი^6.',
+		'answer_must_login' => 'შეკითხვაზე პასუხის გაცემისთვის გთხოვთ, ^1გაიარეთ ავტორიზაცია^2 ან ^3დარეგისტრირდით^4.',
+		'answer_q_popup' => 'უპასუხე ამ შეკითხვას',
+		'approve_a_popup' => 'დაადასტურე ეს პასუხი',
+		'approve_button' => 'დადასტურება',
+		'approve_c_popup' => 'დაადასტურე ეს კომენტარი',
+		'approve_q_popup' => 'დაადასტურე ეს შეკითხვა',
+		'ask_button' => 'დასვი შეკითხვა',
+		'ask_follow_from_a' => 'თქვენი შეკითხვა დაკავშირებული იქნება ამ პასუხთან:',
+		'ask_follow_title' => 'დასვი მსგავსი შეკითხვა',
+		'ask_limit' => 'შეკითხვის დასმის ლიმიტი მიღწეულია - გთხოვთ სცადოთ ხელახლა ერთი საათის შემდეგ',
+		'ask_must_be_approved' => 'შეკითხვის დასმამდე აუცილებელია თქვენი ანგარიშის გააქტიურება.',
+		'ask_must_confirm' => 'შეკითხვის დასმისთვის გთხოვთ, ^5დაადასტურეთ თქვენი ელ-ფოსტის მისამართი^6.',
+		'ask_must_login' => 'შეკითხვის დასმისთვის გთხოვთ გაიაროთ ^1ავტორიზაცია^2 ან ^3რეგისტრაცია^4.',
+		'ask_same_q' => 'სანამ დაიწყებთ, გთხოვთ შეამოწმოთ ხომ არ არის თქვენი შეკითხვა უკვე დასმული:',
+		'ask_title' => 'დასვი შეკითხვა',
+		'c_notify_email' => 'შემატყობინე ამ მისამართზე თუკი ჩემი კომენტარის შემდეგ ახალი დაემატება:',
+		'c_notify_label' => 'შემატყობინე თუკი ჩემი კომენტარის შემდეგ ახალი დაემატება',
+		'c_notify_x_label' => 'მომწერე მეილზე (^) როცა ჩემი კომენტარის შემდეგ ახალი დაემატება',
+		'c_waiting_your_approval' => 'ეს კომენტარი ელოდება თქვენს დასტურს',
+		'c_your_waiting_approval' => 'თქვენი კომენტარი შემოწმდება და დაემატება სულ მალე.',
+		'category_ask_not_allowed' => 'თქვენ არ გაქვთ უფლება შეკითხვა დასვათ ამ კატეგორიაში',
+		'category_js_note' => 'კატეგორიის შესარჩევად აუცილებელია ბრაუზერში ჩართული იყოს Javascript.',
+		'category_required' => 'გთხოვთ აირჩიოთ კატეგორია',
+		'claim_a_popup' => 'Assign this answer to your user account', // სათარგმნი
+		'claim_button' => 'ეს მე დავწერე',
+		'claim_c_popup' => 'Assign this comment to your user account', // სათარგმნი
+		'claim_q_popup' => 'Assign this question to your user account', // სათარგმნი
+		'clear_flags_button' => 'საჩივრების გასუფთავება',
+		'clear_flags_popup' => 'წაშალე საჩივრები ყველა მომხმარებლისგან',
+		'close_button' => 'დახურვა',
+		'close_duplicate' => 'ეს სხვა შეკითხვის ანალოგია',
+		'close_duplicate_error' => 'ანალოგიური შეკითხვა არ მოიძებნა - გთხოვთ სცადეთ ნომრის ჩაწერა სხვა შეკითხვის URL-დან, მაგ. 123.',
+		'close_form_button' => 'შეკითხვის დახურვა',
+		'close_form_title' => 'დახურე ეს შეკითხვა',
+		'close_original_note' => 'თქვენ აგრეთვე შეგიძლიათ შეკითხვის ნომრის ჩაწერა
+URL-დან, მაგ. 123.',
+		'close_original_title' => 'ორიგინალი შეკითხვის URL:',
+		'close_q_popup' => 'დახურე ეს შეკითხვა ახალი პასუხებისთვის',
+		'close_reason_title' => 'შეკითხვის დახურვის მიზეზი:',
+		'closed_as_duplicate' => 'დახურულია როგორც ანალოგი შეკითხვისა:',
+		'closed_with_note' => 'დახურულია შენიშვნით:',
+		'comment_a_popup' => 'დაწერე კომენტარი ამ პასუხზე',
+		'comment_button' => 'დააკომენტარე',
+		'comment_limit' => 'შემოსულია დიდი რაოდენობით კომენტარები - გთხოვთ სცადოთ ხელახლა ერთი საათის შემდეგ',
+		'comment_must_be_approved' => 'კომენტარის დაწერისთვის აუცილებელია, რომ თქვენი ანგარიში იყოს დადასტურებული.',
+		'comment_must_confirm' => 'გთხოვთ, ^5დაადასტურეთ თქვენი ელ-ფოსტის მისამართი^6 რათა დაწეროთ კომენტარი.',
+		'comment_must_login' => 'კომენტარის დასაწერად გაიარეთ ^1ავტორიზაცია^2 ან ^3რეგისტრაცია^4.',
+		'comment_on_a' => 'პასუხზე: ',
+		'comment_on_q' => 'შეკითხვაზე: ',
+		'comment_q_popup' => 'დააკომენტარე ამ შეკითხვაზე',
+		'delete_a_popup' => 'წაშალე ეს პასუხი სამუდამოდ',
+		'delete_button' => 'წაშლა',
+		'delete_c_popup' => 'წაშალე ეს კომენტარი სამუდამოდ',
+		'delete_q_popup' => 'წაშალე ეს შეკითხვა სამუდამოდ',
+		'duplicate_content' => 'Your submission appears to be a duplicate.', // სათარგმნი
+		'edit_a_popup' => 'შეასწორე ეს პასუხი',
+		'edit_a_title' => 'შეასწორე პასუხი',
+		'edit_button' => 'რედაქტირება',
+		'edit_c_popup' => 'ჩაასწორე ეს კომენტარი',
+		'edit_c_title' => 'კომენტარის რედაქტირება',
+		'edit_must_confirm' => 'ამის რედაქტირებისთვის აუცილებელია ^5ელ-ფოსტის მისამართის დადასტურება^6.',
+		'edit_must_login' => 'ამის რედაქტირებისთვის გაიარეთ ^1ავტორიზაცია^2 ან ^3რეგისტრაცია^4.',
+		'edit_q_popup' => 'შეასწორე ეს შეკითხვა',
+		'edit_q_title' => 'შეკითხვის რედაქტირება',
+		'example_tags' => 'თეგების მაგალითები: ',
+		'flag_a_popup' => 'მონიშნე ეს პასუხი როგორც სპამი ან მიუღებელი',
+		'flag_button' => 'გაასაჩივრე',
+		'flag_c_popup' => 'მონიშნე ეს კომენტარი როგორც სპამი ან მიუღებელი',
+		'flag_hide_button' => 'მონიშნე და დამალე',
+		'flag_limit' => 'გასაჩივრების ლიმიტი მიღწეულია - გთხოვთ სცადოთ ხელახლა ერთი საათის შემდეგ',
+		'flag_must_confirm' => 'პოსტების გასაჩივრებისთვის აუცილებელია ^5ელ-ფოსტის მისამართის დადასტურება^6.',
+		'flag_must_login' => 'პოსტების გასაჩივრებისთვის გაიარეთ ^1ავტორიზაცია^2 ან ^3რეგისტრაცია^4.',
+		'flag_not_allowed' => 'ამის გასაჩივრება არ არის ნებადართული',
+		'flag_q_popup' => 'მონიშნე ეს შეკითხვა როგორც სპამი ან მიუღებელი',
+		'follow_a_popup' => 'დასვი ამ პასუხთან დაკავშირებული ახალი შეკითხვა',
+		'follow_button' => 'ask related question', // სათარგმნი
+		'follows_a' => 'related to an answer for:', // სათარგმნი
+		'follows_q' => 'about the question:', // სათარგმნი
+		'hide_a_popup' => 'დამალე ეს პასუხი',
+		'hide_button' => 'დამალვა',
+		'hide_c_popup' => 'დამალე ეს კომენტარი',
+		'hide_q_popup' => 'დამალე ეს შეკითხვა',
+		'matching_tags' => 'მსგავსი თეგები: ',
+		'max_tags_x' => 'დასაშვებია მაქსიმუმ ^ თეგი',
+		'min_tags_x' => 'გთხოვთ გამოიყენეთ სულ მცირე ^ თეგი',
+		'notify_email_note' => 'უსაფრთხოება: თქვენი ელ-ფოსტის მისამართი გამოყენებული იქნება მხოლოდ ამ შეტყობინებების გასაგზავნად.',
+		'q_category_label' => 'კატეგორია:',
+		'q_content_label' => 'მეტი ინფორმაცია შეკითხვაზე, დეტალები:',
+		'q_hidden_author' => 'ეს შეკითხვა დაფარულია მისი ავტორის მიერ',
+		'q_hidden_flagged' => 'ეს შეკითხვა გასაჩივრებული და დაფარულია',
+		'q_hidden_other' => 'ეს შეკითხვა დაფარულია',
+		'q_notify_email' => 'მომივიდეს მეილი ამ მისამართზე როცა შეკითხვაზე დააკომენტარებენ ან უპასუხებენ:',
+		'q_notify_label' => 'მომწერე მეილზე თუ ჩემს შეკითხვას ექნება პასუხი ან კომენტარი',
+		'q_notify_x_label' => 'მომივიდეს მეილზე (^) მიღებული კომენტარები და პასუხები',
+		'q_tags_comma_label' => 'თეგები - გამოიყენეთ მძიმე (,) როგორც გამომყოფი სიმბოლო:',
+		'q_tags_label' => 'თეგები - გამოიყენეთ დეფისი რამდენიმე სიტყვის კომბინაციისას (მაგ: chrome-update):',
+		'q_title_label' => 'შეკითხვა მოკლედ, ერთი წინადადებით:',
+		'q_waiting_approval' => 'ეს შეკითხვა ელოდება დასტურს',
+		'q_waiting_your_approval' => 'ეს შეკითხვა ელოდება თქვენს დასტურს',
+		'q_your_waiting_approval' => 'თქვენი შეკითხვა შემოწმდება და დადასტურდება ძალიან მალე.',
+		'recat_button' => 'რეკატეგორიზაცია',
+		'recat_popup' => 'შეცვალე ამ შეკითხვის კატეგორია',
+		'recat_q_title' => 'შეკითხვის რეკატეგორიზაცია',
+		'reject_a_popup' => 'Reject this answer', // სათარგმნი
+		'reject_button' => 'reject', // სათარგმნი
+		'reject_c_popup' => 'Reject this comment', // სათარგმნი
+		'reject_q_popup' => 'Reject this question', // სათარგმნი
+		'remove_q_favorites' => 'წაშალე ეს შეკითხვა ჩემი ფავორიტებიდან',
+		'reopen_button' => 'ხელახლა გახსნა',
+		'reopen_q_popup' => 'გახსენი ეს შეკითხვა ხელახლა',
+		'reply_button' => 'უპასუხე',
+		'reply_c_popup' => 'უპასუხე ამ კომენტარს',
+		'reshow_a_popup' => 'ხელახლა გამოაჩინე ეს პასუხი',
+		'reshow_button' => 'ხელახლა გამოჩენა',
+		'reshow_c_popup' => 'ხელახლა გამოაჩინე ეს კომენტარი',
+		'reshow_q_popup' => 'ხელახლა გამოაჩინე ეს შეკითხვა',
+		'retag_button' => 'ხელახლა დათაგვა',
+		'retag_cat_popup' => 'შეცვალე ამ შეკითხვის კატეგორია ან თეგები',
+		'retag_popup' => 'შეცვალე ამ შეკითხვის თეგები',
+		'retag_q_title' => 'შეკითხვის ხელახლა დათაგვა',
+		'save_silent_label' => 'შეინახე ჩუმად, რათა არ გამოჩნდეს, რომ რედაქტირება მოხდა',
+		'select_popup' => 'დააჭირეთ, რათა შეირჩეს როგორც საუკეთესო პასუხი',
+		'select_text' => 'საუკეთესო პასუხი',
+		'show_1_comment' => 'მაჩვენე 1 კომენტარი',
+		'show_1_previous_comment' => 'მაჩვენე 1 წინა კომენტარი',
+		'show_x_comments' => 'მაჩვენე ^ კომენტარები',
+		'show_x_previous_comments' => 'მაჩვენე ^ წინა კომენტარები',
+		'unflag_button' => 'საჩივრის გაუქმება',
+		'unflag_popup' => 'გააუქმეთ თქვენი საჩივარი',
+		'unselect_popup' => 'შერჩევის მოსახსნელად დააჭირეთ',
+		'x_answers_title' => '^ პასუხი',
+		'your_answer_title' => 'თქვენი პასუხი',
+		'your_comment_a' => 'თქვენი კომენტარი ამ პასუხზე:',
+		'your_comment_q' => 'თქვენი კომენტარი ამ შეკითხვაზე:',
+	);
+
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
