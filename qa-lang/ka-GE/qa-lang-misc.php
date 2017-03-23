785a65769be914901074243b17480e2b4e065052
Giorgi Tsomaia <comaiagiorgi@gmail.com>
2017-03-23 16:38:38 +0400

    qa-lang-misc.php translation into Georgian
    qa-lang-misc.php translated into Georgian (ka) by me.

---
+++ qa-lang/ka-GE/qa-lang-misc.php
@@ -0,0 +1,126 @@
+<?php
+/*
+	Question2Answer by Gideon Greenspan and contributors
+	http://www.question2answer.org/
+
+	File: qa-include/qa-lang-misc.php
+	Description: Miscellaneous language phrases
+    TRANSLATED INTO GEORGIAN BY GIORGI TSOMAIA
+
+Done:100%
+Checked:0%
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+	return array(
+		'block_ip_button' => 'IP მისამართის დაბლოკვა',
+		'browse_categories' => 'გახსენი კატეგორიები',
+		'captcha_approve_fix' => 'ეს შემოწმება გაქრება როგორც კი თქვენი ანგარიში დასტურს მიიღებს.',
+		'captcha_confirm_fix' => 'მომავალში ამ შემოწმების თავიდან ასაცილებლად გთხოვთ ^5მიუთითეთ თქვენი ელ-ფოსტა^6.',
+		'captcha_error' => 'გთხოვთ გაიაროთ ანტი-სპამ შემოწმება',
+		'captcha_label' => 'ანტი-სპამ შემოწმება:',
+		'captcha_login_fix' => 'მომავალში ამ შემოწმების თავიდან ასაცილებლად გთხოვთ ^1გაიარეთ ავტორიზაცია^2 ან ^3დარეგისტრირდით^4.',
+		'feed_a_edited_prefix' => 'პასუხი ჩასწორებულია: ',
+		'feed_a_prefix' => 'პასუხგაცემულია: ',
+		'feed_a_reshown_prefix' => 'პასუხი კვლავ გამოჩენილია: ',
+		'feed_a_selected_prefix' => 'პასუხი შერჩეულია: ',
+		'feed_c_edited_prefix' => 'კომენტარი ჩასწორებულია: ',
+		'feed_c_moved_prefix' => 'კომენტარი გადატანილია: ',
+		'feed_c_prefix' => 'დაკომენტარებულია: ',
+		'feed_c_reshown_prefix' => 'კომენტარი კვლავ გამოჩენილია: ',
+		'feed_closed_prefix' => 'დახურული: ',
+		'feed_edited_prefix' => 'ჩასწორებული: ',
+		'feed_hidden_prefix' => 'დამალული: ',
+		'feed_not_found' => 'Feed არ არის ნაპოვნი',
+		'feed_recategorized_prefix' => 'რეკატეგორიზებული: ',
+		'feed_reopened_prefix' => 'ხელახლა გაიხსნა: ',
+		'feed_reshown_prefix' => 'კვლავ გამოჩენილია: ',
+		'feed_retagged_prefix' => 'ხელახლა დათაგულია: ',
+		'feedback_email' => 'თქვენი ელ-ფოსტა: (არასავალდებულო)',
+		'feedback_empty' => 'გთხოვთ გამოიყენოთ ეს ველი კომენტარებისა და რჩევების გამოსაგზავნად',
+		'feedback_message' => 'თქვენი კომენტარი ან რჩევა ^ -ისთვის:',
+		'feedback_name' => 'თქვენი სახელი: (არასავალდებულო)',
+		'feedback_sent' => 'თქვენი მესიჯი გაიგზავნა - გმადლობთ.',
+		'feedback_title' => 'უკუკავშირი',
+		'form_security_again' => 'დასადასტურებლად დააჭირეთ კიდევ ერთხელ',
+		'form_security_reload' => 'გთხოვთ დაარეფრეშეთ გვერდი და თავიდან სცადეთ',
+		'hide_all_ip_button' => 'დამალე ყველა პოსტი ამ IP მისამართისგან',
+		'host_name' => 'ჰოსტის სახელი:',
+		'inbox' => 'შემომავალი',
+		'matches_blocked_ips' => 'ემთხვევა დაბლოკილ IP მისამართებს:',
+		'message_empty' => 'ჩაწერეთ მესიჯის ტექსტი რათა გაუგზავნოთ ამ მომხმარებელს',
+		'message_explanation' => 'გაიგზავნება როგორც შეტყობინება ^ -გან. თქვენი ელ-ფოსტა არ გამოჩნდება თუკი მას არ ჩაწერთ თავად მესიჯში.',
+		'message_for_x' => 'მესიჯი ^:',
+		'message_limit' => 'თქვენ არ შეგიძლიათ მეტი პირადი მესიჯის გაგზავნა 1 საათის განმავლობაში',
+		'message_must_login' => 'პირადი მესიჯის გასაგზავნად გთხოვთ ^1გაიარეთ ავტორიზაცია^2 ან ^3რეგისტრაცია^4.',
+		'message_recent_history' => 'ბოლო მიმოწერა ^ -თან',
+		'message_sent' => 'თქვენი პირადი მესიჯი გაიგზავნა',
+		'more_favorite_qs' => 'მეტი რჩეული შეკითხვა...',
+		'more_favorite_tags' => 'მეტი რჩეული თეგი...',
+		'more_favorite_users' => 'მეტი რჩეული მომხმარებელი...',
+		'my_favorites_title' => 'ჩემი ფავორიტები',
+		'nav_all_my_updates' => 'ჩემი სიახლეები',
+		'nav_my_content' => 'ჩემი კონტენტი',
+		'nav_my_details' => 'ჩემი ანგარიში',
+		'nav_my_favorites' => 'ჩემი ფავორიტები',
+		'nav_user_activity' => 'ბოლო აქტივობა',
+		'nav_user_as' => 'ყველა პასუხი',
+		'nav_user_pms' => 'პირადი მესიჯები',
+		'nav_user_qs' => 'ყველა შეკითხვა',
+		'nav_user_wall' => 'კედელი',
+		'no_activity_from_x' => 'აქტივობა მომხმარებლისგან ^ არ არის',
+		'no_favorite_categories' => 'რჩეული კატეგორიები არ არის',
+		'no_favorite_qs' => 'რჩეული შეკითხვები არ არის',
+		'no_favorite_tags' => 'რჩეული თეგები არ არის',
+		'no_favorite_users' => 'რჩეული მომხმარებლები არ არის',
+		'no_recent_updates' => 'უახლესი ცვლილებები არ არის',
+		'no_updates_content' => 'უახლესი ცვლილებები ჩემი კონტენტისთვის არ არის',
+		'no_updates_favorites' => 'ჩემი რჩეულებისთვის ცვლილებები არ არის',
+		'outbox' => 'გაგზავნილი',
+		'pm_inbox_title' => 'შემოსული პირადი მესიჯები',
+		'pm_outbox_title' => 'გაგზავნილი პირადი მესიჯები',
+		'private_message_title' => 'გაგზავნე პირადი მესიჯი',
+		'recent_activity_from_x' => 'ბოლო აქტივობა მომხმარებლისგან - ^',
+		'recent_updates_content' => 'ბოლო სიახლეები ჩემი კონტენტისთვის',
+		'recent_updates_favorites' => 'ბოლო სიახლეები ჩემს ფავორიტებში',
+		'recent_updates_title' => 'ჩემი ბოლო სიახლეები',
+		'site_in_maintenance' => 'საიტი ამჟამად გათიშულია განახლების გამო - გთხოვთ, მოგვიანებით გვეწვიეთ.',
+		'suggest_favorites_add' => 'შეკითხვის ან სხვა რამის ფავორიტებში დასამატებლად დააჭირეთ ^ სიმბოლოს.',
+		'suggest_update_favorites' => 'მეტი სიახლეებისთვის, დაამატეთ ^1ფავორიტები^2.',
+		'unblock_ip_button' => 'IP მისამართის განბლოკვა',
+		'your_a_commented' => 'თქვენს პასუხზე დააკომენტარეს',
+		'your_a_edited' => 'თქვენი პასუხი ჩასწოებულია',
+		'your_a_hidden' => 'თქვენი პასუხი დამალულია',
+		'your_a_questioned' => 'შეკითხვა დასვა თქვენს პასუხზე',
+		'your_a_reshown' => 'თქვენი პასუხი კვლავ ჩანს',
+		'your_a_selected' => 'თქვენი პასუხი შერჩეულია',
+		'your_c_edited' => 'თქვენი კომენტარი ჩასწორებულია',
+		'your_c_followed' => 'თქვენს კომენტარს ახალი დამკვირვებელი ჰყავს',
+		'your_c_hidden' => 'თქვენი კომენტარი დამალულია',
+		'your_c_moved' => 'თქვენი კომენტარი გადატანილია',
+		'your_c_reshown' => 'თქვენი კომენტარი კვლავ ჩანს',
+		'your_q_answered' => 'თქვენს შეკითხვას უპასუხეს',
+		'your_q_closed' => 'თქვენი შეკითხვა დახურულია',
+		'your_q_commented' => 'თქვენს შეგითხვაზე დააკომენტარეს',
+		'your_q_edited' => 'თქვენი შეკითხვა ჩასწორებულია',
+		'your_q_hidden' => 'თქვენი შეკითხვა დამალულია',
+		'your_q_recategorized' => 'თქვენს შეკითხვას შეეცვალა კატეგორია',
+		'your_q_reopened' => 'თქვენი შეკითხვა ხელახლა გაიხსნა',
+		'your_q_reshown' => 'თქვენი შეკითხვა კვლავ ჩანს',
+		'your_q_retagged' => 'თქვენს შეკითხვას შეეცვალა თეგები',
+	);
+
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
