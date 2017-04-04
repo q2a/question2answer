c8e3ee70f45e88b0da9fdf40692b57e092d15949
Scott <scott.vivian@gmail.com>
2017-04-04 13:37:27 +0100

    Prevent DB connection in unit tests

--- qa-tests/autoload.php
+++ qa-tests/autoload.php
@@ -6,4 +6,5 @@ global $qa_options_cache;
 // Needed in order to avoid accessing the database while including the qa-base.php file
 $qa_options_cache['enabled_plugins'] = '';
 
+$qa_autoconnect = false;
 require_once __DIR__.'/../qa-include/qa-base.php';
