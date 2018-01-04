2dfe487311e739b6a2cad63dd558deb8ea6681dd
pupi1985 <pupi1985@bitbucket.org>
2018-01-04 02:20:15 -0300

    Refactor UserPosts controller removing checks for external users and generalizing code

--- qa-src/Controllers/User/UserPosts.php
+++ qa-src/Controllers/User/UserPosts.php
@@ -27,9 +27,6 @@ require_once QA_INCLUDE_DIR . 'app/format.php';
 
 class UserPosts extends \Q2A\Controllers\BaseController
 {
-	protected $userid;
-	protected $userhtml;
-
 	/**
 	 * @param string $handle
 	 *
@@ -38,69 +35,54 @@ class UserPosts extends \Q2A\Controllers\BaseController
 	 */
 	public function activity($handle)
 	{
-		$this->userHtml($handle);
+		$profileUserId = $this->getUserIdOrFail($handle);
 
 		// Find the recent activity for this user
 
-		$loginuserid = qa_get_logged_in_userid();
-		$identifier = QA_FINAL_EXTERNAL_USERS ? $this->userid : $handle;
+		$loggedInUserId = qa_get_logged_in_userid();
 
-		list($useraccount, $questions, $answerqs, $commentqs, $editqs) = qa_db_select_with_pending(
-			QA_FINAL_EXTERNAL_USERS ? null : qa_db_user_account_selectspec($handle, false),
-			qa_db_user_recent_qs_selectspec($loginuserid, $identifier, qa_opt_if_loaded('page_size_activity')),
-			qa_db_user_recent_a_qs_selectspec($loginuserid, $identifier),
-			qa_db_user_recent_c_qs_selectspec($loginuserid, $identifier),
-			qa_db_user_recent_edit_qs_selectspec($loginuserid, $identifier)
-		);
+		list($questions, $answerQs, $commentQs, $editQs) = $this->fetchActivityDataFromDb($handle, $loggedInUserId, $profileUserId);
 
-		if (!QA_FINAL_EXTERNAL_USERS && !is_array($useraccount)) { // check the user exists
-			throw new PageNotFoundException();
-		}
 		// Get information on user references
 
-		$questions = qa_any_sort_and_dedupe(array_merge($questions, $answerqs, $commentqs, $editqs));
+		$questions = qa_any_sort_and_dedupe(array_merge($questions, $answerQs, $commentQs, $editQs));
 		$questions = array_slice($questions, 0, qa_opt('page_size_activity'));
-		$usershtml = qa_userids_handles_html(qa_any_get_userids_handles($questions), false);
-
+		$usersHtml = qa_userids_handles_html(qa_any_get_userids_handles($questions), false);
 
 		// Prepare content for theme
 
 		$qa_content = qa_content_prepare(true);
 
-		if (count($questions))
-			$qa_content['title'] = qa_lang_html_sub('profile/recent_activity_by_x', $this->userhtml);
-		else
-			$qa_content['title'] = qa_lang_html_sub('profile/no_posts_by_x', $this->userhtml);
-
+		$qa_content['title'] = qa_lang_html_sub(
+			empty($questions) ? 'profile/no_posts_by_x' : 'profile/recent_activity_by_x',
+			$this->userHtml($handle, $profileUserId)
+		);
 
 		// Recent activity by this user
 
-		$qa_content['q_list']['form'] = array(
-			'tags' => 'method="post" action="' . qa_self_html() . '"',
-
-			'hidden' => array(
-				'code' => qa_get_form_security_code('vote'),
-			),
-		);
+		$this->addForm($qa_content);
 
 		$qa_content['q_list']['qs'] = array();
 
-		$htmldefaults = qa_post_html_defaults('Q');
-		$htmldefaults['whoview'] = false;
-		$htmldefaults['voteview'] = false;
-		$htmldefaults['avatarsize'] = 0;
+		$htmlDefaults = qa_post_html_defaults('Q');
+		$htmlDefaults['whoview'] = false;
+		$htmlDefaults['voteview'] = false;
+		$htmlDefaults['avatarsize'] = 0;
 
 		foreach ($questions as $question) {
-			$qa_content['q_list']['qs'][] = qa_any_to_q_html_fields($question, $loginuserid, qa_cookie_get(),
-				$usershtml, null, array('voteview' => false) + qa_post_html_options($question, $htmldefaults));
+			$qa_content['q_list']['qs'][] = qa_any_to_q_html_fields(
+				$question,
+				$loggedInUserId,
+				qa_cookie_get(),
+				$usersHtml,
+				null,
+				array('voteview' => false) + qa_post_html_options($question, $htmlDefaults)
+			);
 		}
-
 
 		// Sub menu for navigation in user pages
 
-		$ismyuser = isset($loginuserid) && $loginuserid == (QA_FINAL_EXTERNAL_USERS ? $this->userid : $useraccount['userid']);
-		$qa_content['navigation']['sub'] = qa_user_sub_navigation($handle, 'activity', $ismyuser);
-
+		$this->addNavigation($qa_content, $handle, $loggedInUserId, $profileUserId, 'activity');
 
 		return $qa_content;
 	}
@@ -113,73 +95,53 @@ class UserPosts extends \Q2A\Controllers\BaseController
 	 */
 	public function questions($handle)
 	{
-		$this->userHtml($handle);
-
-		$start = qa_get_start();
+		$profileUserId = $this->getUserIdOrFail($handle);
 
 		// Find the questions for this user
 
-		$loginuserid = qa_get_logged_in_userid();
-		$identifier = QA_FINAL_EXTERNAL_USERS ? $this->userid : $handle;
+		$loggedInUserId = qa_get_logged_in_userid();
 
-		list($useraccount, $userpoints, $questions) = qa_db_select_with_pending(
-			QA_FINAL_EXTERNAL_USERS ? null : qa_db_user_account_selectspec($handle, false),
-			qa_db_user_points_selectspec($identifier),
-			qa_db_user_recent_qs_selectspec($loginuserid, $identifier, qa_opt_if_loaded('page_size_qs'), $start)
-		);
-
-		if (!QA_FINAL_EXTERNAL_USERS && !is_array($useraccount)) { // check the user exists
-			throw new PageNotFoundException();
-		}
-
+		list($userPoints, $questions) = $this->fetchQuestionsDataFromDb($handle, $loggedInUserId, $profileUserId);
 
 		// Get information on user questions
 
-		$pagesize = qa_opt('page_size_qs');
-		$count = (int)@$userpoints['qposts'];
-		$questions = array_slice($questions, 0, $pagesize);
-		$usershtml = qa_userids_handles_html($questions, false);
-
+		$pageSize = qa_opt('page_size_qs');
+		$count = (int)@$userPoints['qposts'];
+		$questions = array_slice($questions, 0, $pageSize);
+		$usersHtml = qa_userids_handles_html($questions, false);
 
 		// Prepare content for theme
 
 		$qa_content = qa_content_prepare(true);
 
-		if (count($questions))
-			$qa_content['title'] = qa_lang_html_sub('profile/questions_by_x', $this->userhtml);
-		else
-			$qa_content['title'] = qa_lang_html_sub('profile/no_questions_by_x', $this->userhtml);
-
+		$qa_content['title'] = qa_lang_html_sub(
+			empty($questions) ? 'profile/no_questions_by_x' : 'profile/questions_by_x',
+			$this->userHtml($handle, $profileUserId)
+		);
 
 		// Recent questions by this user
 
-		$qa_content['q_list']['form'] = array(
-			'tags' => 'method="post" action="' . qa_self_html() . '"',
-
-			'hidden' => array(
-				'code' => qa_get_form_security_code('vote'),
-			),
-		);
+		$this->addForm($qa_content);
 
 		$qa_content['q_list']['qs'] = array();
 
-		$htmldefaults = qa_post_html_defaults('Q');
-		$htmldefaults['whoview'] = false;
-		$htmldefaults['avatarsize'] = 0;
+		$htmlDefaults = qa_post_html_defaults('Q');
+		$htmlDefaults['whoview'] = false;
+		$htmlDefaults['avatarsize'] = 0;
 
 		foreach ($questions as $question) {
-			$qa_content['q_list']['qs'][] = qa_post_html_fields($question, $loginuserid, qa_cookie_get(),
-				$usershtml, null, qa_post_html_options($question, $htmldefaults));
+			$qa_content['q_list']['qs'][] = qa_post_html_fields(
+				$question,
+				$loggedInUserId,
+				qa_cookie_get(),
+				$usersHtml,
+				null,
+				qa_post_html_options($question, $htmlDefaults)
+			);
 		}
 
-		$qa_content['page_links'] = qa_html_page_links(qa_request(), $start, $pagesize, $count, qa_opt('pages_prev_next'));
-
-
-		// Sub menu for navigation in user pages
-
-		$ismyuser = isset($loginuserid) && $loginuserid == (QA_FINAL_EXTERNAL_USERS ? $this->userid : $useraccount['userid']);
-		$qa_content['navigation']['sub'] = qa_user_sub_navigation($handle, 'questions', $ismyuser);
-
+		$this->addPageLinks($qa_content, $pageSize, $count);
+		$this->addNavigation($qa_content, $handle, $loggedInUserId, $profileUserId, 'questions');
 
 		return $qa_content;
 	}
@@ -192,79 +154,60 @@ class UserPosts extends \Q2A\Controllers\BaseController
 	 */
 	public function answers($handle)
 	{
-		$this->userHtml($handle);
-
-		$start = qa_get_start();
-
+		$profileUserId = $this->getUserIdOrFail($handle);
 
 		// Find the questions for this user
 
-		$loginuserid = qa_get_logged_in_userid();
-		$identifier = QA_FINAL_EXTERNAL_USERS ? $this->userid : $handle;
+		$loggedInUserId = qa_get_logged_in_userid();
 
-		list($useraccount, $userpoints, $questions) = qa_db_select_with_pending(
-			QA_FINAL_EXTERNAL_USERS ? null : qa_db_user_account_selectspec($handle, false),
-			qa_db_user_points_selectspec($identifier),
-			qa_db_user_recent_a_qs_selectspec($loginuserid, $identifier, qa_opt_if_loaded('page_size_activity'), $start)
-		);
-
-		if (!QA_FINAL_EXTERNAL_USERS && !is_array($useraccount)) { // check the user exists
-			throw new PageNotFoundException();
-		}
-
+		list($userPoints, $questions) = $this->fetchAnswersDataFromDb($handle, $loggedInUserId, $profileUserId);
 
 		// Get information on user questions
 
 		$pagesize = qa_opt('page_size_activity');
-		$count = (int)@$userpoints['aposts'];
+		$count = (int)@$userPoints['aposts'];
 		$questions = array_slice($questions, 0, $pagesize);
-		$usershtml = qa_userids_handles_html($questions, false);
-
+		$usersHtml = qa_userids_handles_html($questions, false);
 
 		// Prepare content for theme
 
 		$qa_content = qa_content_prepare(true);
 
-		if (count($questions))
-			$qa_content['title'] = qa_lang_html_sub('profile/answers_by_x', $this->userhtml);
-		else
-			$qa_content['title'] = qa_lang_html_sub('profile/no_answers_by_x', $this->userhtml);
-
+		$qa_content['title'] = qa_lang_html_sub(
+			empty($questions) ? 'profile/no_answers_by_x' : 'profile/answers_by_x',
+			$this->userHtml($handle, $profileUserId)
+		);
 
 		// Recent questions by this user
 
-		$qa_content['q_list']['form'] = array(
-			'tags' => 'method="post" action="' . qa_self_html() . '"',
-
-			'hidden' => array(
-				'code' => qa_get_form_security_code('vote'),
-			),
-		);
+		$this->addForm($qa_content);
 
 		$qa_content['q_list']['qs'] = array();
 
-		$htmldefaults = qa_post_html_defaults('Q');
-		$htmldefaults['whoview'] = false;
-		$htmldefaults['avatarsize'] = 0;
-		$htmldefaults['ovoteview'] = true;
-		$htmldefaults['answersview'] = false;
+		$htmlDefaults = qa_post_html_defaults('Q');
+		$htmlDefaults['whoview'] = false;
+		$htmlDefaults['avatarsize'] = 0;
+		$htmlDefaults['ovoteview'] = true;
+		$htmlDefaults['answersview'] = false;
+
+		$voteView = qa_get_vote_view('A', false, false);
 
 		foreach ($questions as $question) {
-			$options = qa_post_html_options($question, $htmldefaults);
-			$options['voteview'] = qa_get_vote_view('A', false, false);
+			$options = qa_post_html_options($question, $htmlDefaults);
+			$options['voteview'] = $voteView;
 
-			$qa_content['q_list']['qs'][] = qa_other_to_q_html_fields($question, $loginuserid, qa_cookie_get(),
-				$usershtml, null, $options);
+			$qa_content['q_list']['qs'][] = qa_other_to_q_html_fields(
+				$question,
+				$loggedInUserId,
+				qa_cookie_get(),
+				$usersHtml,
+				null,
+				$options
+			);
 		}
 
-		$qa_content['page_links'] = qa_html_page_links(qa_request(), $start, $pagesize, $count, qa_opt('pages_prev_next'));
-
-
-		// Sub menu for navigation in user pages
-
-		$ismyuser = isset($loginuserid) && $loginuserid == (QA_FINAL_EXTERNAL_USERS ? $this->userid : $useraccount['userid']);
-		$qa_content['navigation']['sub'] = qa_user_sub_navigation($handle, 'answers', $ismyuser);
-
+		$this->addPageLinks($qa_content, $pagesize, $count);
+		$this->addNavigation($qa_content, $handle, $loggedInUserId, $profileUserId, 'answers');
 
 		return $qa_content;
 	}
@@ -273,19 +216,140 @@ class UserPosts extends \Q2A\Controllers\BaseController
 	 * Return the HTML to display for the handle, and if we're using external users, determine the userid.
 	 *
 	 * @param string $handle
-	 * @throws PageNotFoundException
+	 * @param mixed $userId
+	 *
+	 * @return mixed|string
 	 */
-	private function userHtml($handle)
+	private function userHtml($handle, $userId)
 	{
 		if (QA_FINAL_EXTERNAL_USERS) {
-			$this->userid = qa_handle_to_userid($handle);
-			if (!isset($this->userid)) { // check the user exists
-				throw new PageNotFoundException();
-			}
+			$usersHtml = qa_get_users_html(array($userId), false, qa_path_to_root(), true);
 
-			$usershtml = qa_get_users_html(array($this->userid), false, qa_path_to_root(), true);
-			$this->userhtml = @$usershtml[$this->userid];
-		} else
-			$this->userhtml = qa_html($handle);
+			return @$usersHtml[$userId];
+		} else {
+			return qa_html($handle);
+		}
+	}
+
+	/**
+	 * @param string $handle
+	 * @param mixed $loggedInUserId
+	 * @param mixed $userId
+	 *
+	 * @return array
+	 */
+	private function fetchActivityDataFromDb($handle, $loggedInUserId, $userId)
+	{
+		$identifier = $this->getGenericUserIdentifier($userId, $handle);
+
+		return qa_db_select_with_pending(
+			qa_db_user_recent_qs_selectspec($loggedInUserId, $identifier, qa_opt_if_loaded('page_size_activity')),
+			qa_db_user_recent_a_qs_selectspec($loggedInUserId, $identifier),
+			qa_db_user_recent_c_qs_selectspec($loggedInUserId, $identifier),
+			qa_db_user_recent_edit_qs_selectspec($loggedInUserId, $identifier)
+		);
+	}
+
+	/**
+	 * @param string $handle
+	 * @param mixed $loginuserid
+	 * @param mixed $userId
+	 *
+	 * @return array
+	 */
+	private function fetchQuestionsDataFromDb($handle, $loginuserid, $userId)
+	{
+		$identifier = $this->getGenericUserIdentifier($userId, $handle);
+
+		return qa_db_select_with_pending(
+			qa_db_user_points_selectspec($identifier),
+			qa_db_user_recent_qs_selectspec($loginuserid, $identifier, qa_opt_if_loaded('page_size_qs'), qa_get_start())
+		);
+	}
+
+	/**
+	 * @param string $handle
+	 * @param mixed $loggedInUserId
+	 * @param mixed $userId
+	 *
+	 * @return array
+	 */
+	private function fetchAnswersDataFromDb($handle, $loggedInUserId, $userId)
+	{
+		$identifier = $this->getGenericUserIdentifier($userId, $handle);
+
+		return qa_db_select_with_pending(
+			qa_db_user_points_selectspec($identifier),
+			qa_db_user_recent_a_qs_selectspec($loggedInUserId, $identifier, qa_opt_if_loaded('page_size_activity'), qa_get_start())
+		);
+	}
+
+	/**
+	 * @param string $handle
+	 *
+	 * @return mixed
+	 * @throws PageNotFoundException
+	 */
+	private function getUserIdOrFail($handle)
+	{
+		$userId = qa_handle_to_userid($handle);
+
+		if (is_null($userId)) { // check the user exists
+			throw new PageNotFoundException();
+		}
+
+		return $userId;
+	}
+
+	/**
+	 * @param mixed $userId
+	 * @param string $handle
+	 *
+	 * @return mixed
+	 */
+	private function getGenericUserIdentifier($userId, $handle)
+	{
+		return QA_FINAL_EXTERNAL_USERS ? $userId : $handle;
+	}
+
+	/**
+	 * Add page links and sub menu for navigation in user pages.
+	 *
+	 * @param array $qa_content
+	 * @param int $pageSize
+	 * @param int $count
+	 */
+	private function addPageLinks(&$qa_content, $pageSize, $count)
+	{
+		$qa_content['page_links'] = qa_html_page_links(qa_request(), qa_get_start(), $pageSize, $count, qa_opt('pages_prev_next'));
+	}
+
+	/**
+	 * Add sub menu for navigation in user pages to the qa_content array.
+	 *
+	 * @param array $qa_content
+	 * @param string $handle
+	 * @param mixed $loggedInUserId
+	 * @param mixed $userId
+	 * @param string $selectedNavigation
+	 */
+	private function addNavigation(&$qa_content, $handle, $loggedInUserId, $userId, $selectedNavigation)
+	{
+		$isMyUser = isset($loggedInUserId) && $loggedInUserId == $userId;
+		$qa_content['navigation']['sub'] = qa_user_sub_navigation($handle, $selectedNavigation, $isMyUser);
+	}
+
+	/**
+	 * @param array $qa_content
+	 */
+	private function addForm(&$qa_content)
+	{
+		$qa_content['q_list']['form'] = array(
+			'tags' => 'method="post" action="' . qa_self_html() . '"',
+
+			'hidden' => array(
+				'code' => qa_get_form_security_code('vote'),
+			),
+		);
 	}
 }
