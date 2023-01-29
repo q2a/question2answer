26181f7f4988b1b5aaf380f65ecb3c86125066c8
Emran <emran.batmanghelich@divar.ir>
2023-01-29 11:25:51 +0330

    Feat: Make post content direction auto
    Q2A already supports RTL direction in its settings. But, when switching
    to a direction (e.g. RTL) the contents of the other direction (LTR) are
    messed up.
    Hence, in order to support mixed directions, `dir=auto` got added to the
    post content `div`s.

--- qa-include/qa-theme-base.php
+++ qa-include/qa-theme-base.php
@@ -743,7 +743,7 @@ class qa_html_theme_base
 			if (isset($favorite))
 				$this->output('<form ' . $favorite['form_tags'] . '>');
 
-			$this->output('<div class="qa-main-heading">');
+			$this->output('<div class="qa-main-heading" dir="auto">');
 			$this->favorite();
 			$this->output('<h1>');
 			$this->title();
@@ -1713,7 +1713,7 @@ class qa_html_theme_base
 	public function q_item_title($q_item)
 	{
 		$this->output(
-			'<div class="qa-q-item-title">',
+			'<div class="qa-q-item-title" dir="auto">',
 			'<a href="' . $q_item['url'] . '">' . $q_item['title'] . '</a>',
 			// add closed note in title
 			empty($q_item['closed']['state']) ? '' : ' [' . $q_item['closed']['state'] . ']',
@@ -2198,7 +2198,7 @@ class qa_html_theme_base
 	{
 		$content = isset($q_view['content']) ? $q_view['content'] : '';
 
-		$this->output('<div class="qa-q-view-content qa-post-content">');
+		$this->output('<div class="qa-q-view-content qa-post-content" dir="auto">');
 		$this->output_raw($content);
 		$this->output('</div>');
 	}
@@ -2361,7 +2361,7 @@ class qa_html_theme_base
 			$a_item['content'] = '';
 		}
 
-		$this->output('<div class="qa-a-item-content qa-post-content">');
+		$this->output('<div class="qa-a-item-content qa-post-content" dir="auto">');
 		$this->output_raw($a_item['content']);
 		$this->output('</div>');
 	}
